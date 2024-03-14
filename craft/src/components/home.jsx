299f05061d4c65d9db5663c6057c3dc35347a7c5
kagabojaphet <iradukundajaphet59@gmail.com>
2024-03-14 16:40:50 +0200

    add

--- craft/src/components/home.jsx
+++ craft/src/components/home.jsx
@@ -1,8 +1,38 @@
 import React from "react";
+import mtn from "../assets/kk.png";
+import pencil from "../assets/pencil.png";
+import notbook from "../assets/Notebook.png";
+import parents from "../assets/Parents.png";
+import hand from "../assets/Hand.png";
+import valentine from "../assets/Valentine.png";
+
 const Home=()=>{
     return(
         <>
         <div className="home-container">
+            <div className="home-image">
+            <img src={mtn} alt="" />
+            <div className="home-text">
+                <div className="home-text-text">
+                    <p className="p-one">Ignite the Spark of Imagination:</p>
+                    <p className="p-two"><span>MasterKraft's</span> Secrets to</p>
+                    <p className="p-three">Nurturing <span>Young Talent</span> and</p>
+                    <p className="p-four">Cultivating Lifelong Skills for a</p>
+                    <p className="p-five"><span>Bright and</span> Exciting Future.</p>
+                    <div className="btn">
+                    <button>EXPLORE THE BOX</button>
+                    </div>
+                </div>
+                <div className="home-text-carton">
+                    <img src={pencil} alt="" />
+                    <img src={parents} alt="" />
+                    <img src={valentine} alt="" />
+                    <img src={hand} alt="" />
+                    <img src={notbook} alt="" />
+                </div>
+            </div>
+            </div>
+           
             <div className="home-page">
             </div>
         </div>
