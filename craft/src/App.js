879c71567f58077b6da83387fd80ccdf8823a703
kagabojaphet <iradukundajaphet59@gmail.com>
2024-03-14 13:05:16 +0200

    add

--- craft/src/App.js
+++ craft/src/App.js
@@ -2,11 +2,14 @@
 import React from "react";
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 import Welcome from "./pages/welcome";
+import Homes from "./pages/home";
+import './sass/main.scss'
 function App() {
   return (
    <Router>
     <Routes>
       <Route path="/" element={<Welcome/>}></Route>
+      <Route path="/Home" element={<Homes/>} ></Route>
     </Routes>
    </Router>
   );
