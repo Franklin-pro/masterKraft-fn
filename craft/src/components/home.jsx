4d1ccd1d2c9dc63297bbbd117b66812053b0743a
kagabojaphet <iradukundajaphet59@gmail.com>
2024-03-15 16:06:02 +0200

    add

--- craft/src/components/home.jsx
+++ craft/src/components/home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import mtn from "../assets/kk.png";
 import pencil from "../assets/pencil.png";
-import notbook from "../assets/Notebook.png";
+import kk from "../assets/kkk.png";
 import parents from "../assets/Parents.png";
 import hand from "../assets/Hand.png";
 import valentine from "../assets/Valentine.png";
+import bike from "../assets/bike.svg";
+import smiley from "../assets/smiley-face.svg";
+import brid from "../assets/brid.svg"
 
 const Home=()=>{
     return(
@@ -12,6 +15,15 @@ const Home=()=>{
         <div className="home-container">
             <div className="home-image">
             <img src={mtn} alt="" />
+            <div className="home-text-carton">
+                    <img src={pencil} alt="" className="pencil"/>
+                    <img src={parents} alt="" className="parents"/>
+                    <img src={valentine} alt="" className="valentine"/>
+                    <img src={hand} alt="" className="hand"/>
+                    <img src={bike} alt="" className="bike"/>
+                    <img src={smiley} alt="" className="smiley"/>
+                    <img src={brid} alt="" className="brid"/>
+                </div>
             <div className="home-text">
                 <div className="home-text-text">
                     <p className="p-one">Ignite the Spark of Imagination:</p>
@@ -23,17 +35,37 @@ const Home=()=>{
                     <button>EXPLORE THE BOX</button>
                     </div>
                 </div>
-                <div className="home-text-carton">
-                    <img src={pencil} alt="" />
-                    <img src={parents} alt="" />
-                    <img src={valentine} alt="" />
-                    <img src={hand} alt="" />
-                    <img src={notbook} alt="" />
+            </div>
+            </div>
+            <div className="home-bottom">
+                <div className="home-bottom-img">
+                    <img src={kk} alt="" />
                 </div>
-            </div>
-            </div>
-           
-            <div className="home-page">
+                <div className="home-bottom-txt">
+                    <h2><span>Holiday Course</span> Countdown</h2>
+                    <p>Exciting learning adventures for festive seasons!</p>
+                </div>
+                <div className="home-bottom-date">
+                    <div className="days">
+                        <h1>69</h1>
+                        <p>Days</p>
+                    </div>
+                    <p>:</p>
+                    <div className="hours">
+                        <h1>09</h1>
+                        <p>hours</p>
+                    </div>
+                    <p>:</p>
+                    <div className="minutes">
+                        <h1>59</h1>
+                        <p>minutes</p>
+                    </div>
+                    <p>:</p>
+                    <div className="seconds">
+                        <h1>60</h1>
+                        <p>seconds</p>
+                    </div>
+                </div>
             </div>
         </div>
         </>
--- craft/src/pages/home.jsx
+++ craft/src/pages/home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Home from "../components/home";
 
+
 const Homes=()=>{
     return(
         <>
